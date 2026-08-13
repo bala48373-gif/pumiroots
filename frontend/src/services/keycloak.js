0699dac582e2280deebbaad7aa3b3f534ad88145
Bala Murugan <bala48373@gmail.com>
2026-08-13 20:06:31 +0530

    point frontend keycloak config to cloud keycloak instance

--- frontend/src/services/keycloak.js
+++ frontend/src/services/keycloak.js
@@ -1,7 +1,7 @@
 import Keycloak from "keycloak-js";
 
 const keycloak = new Keycloak({
-  url: "http://localhost:8080",
+  url: "https://pumiroots-keycloak.onrender.com",
   realm: "pumiroots",
   clientId: "pumiroots-frontend",
 });
